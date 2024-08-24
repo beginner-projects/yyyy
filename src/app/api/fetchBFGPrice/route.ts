3a13249552bdfc9ac38e3d5b62434e4ff7e8ebfb
nick <nickj8366@gmail.com>
2024-08-24 20:35:36 +0530

    Update route.ts

--- src/app/api/fetchBFGPrice/route.ts
+++ src/app/api/fetchBFGPrice/route.ts
@@ -5,7 +5,7 @@ import { NextResponse } from 'next/server';
 
 export async function GET() {
   try {
-    const isLocal = process.env.NODE_ENV === 'development';
+    const isLocal = 'dev';
     const ePath = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe";
     
     const browser = await puppeteer.launch({
